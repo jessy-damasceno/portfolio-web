f7dc86833c94cdfddc8d89571c912aa8390d33c7
Lucas Otoni <lotoni.vivo@gmail.com>
2023-03-09 15:37:56 -0300

    feature: routes created

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import Home from '../components/Home.vue'
-import About from '../components/About.vue'
+import * as components from '../views'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,12 +7,37 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: Home
+      component: components.Home
     },
     {
       path: '/about',
       name: 'about',
-      component: About
+      component: components.About
+    },
+    {
+      path: '/skills',
+      name: 'skills',
+      component: components.Skills
+    },
+    {
+      path: '/experience',
+      name: 'experience',
+      component: components.Experience
+    },
+    {
+      path: '/education',
+      name: 'education',
+      component: components.Education
+    },
+    {
+      path: '/portfolio',
+      name: 'portfolio',
+      component: components.Portfolio
+    },
+    {
+      path: '/contact',
+      name: 'contact',
+      component: components.Contact
     },
   ]
 })
