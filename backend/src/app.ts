098313149c69a7284a2e2860b9070cd3bc9d91d1
Jessy Damasceno <jessy_bas@hotmail.com>
2023-03-24 17:24:24 -0300

    feat: implements CORS in API

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import cors from 'cors';
 import ErrorHandler from './middlewares/ErrorHandler';
 import userRouter from './Routes/user.route';
 import projectRouter from './Routes/project.route';
@@ -6,6 +7,7 @@ import swaggerRouter from './Routes/swagger.route';
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 app.use('/user', userRouter);
 app.use('/project', projectRouter);
