260c74bfe002f567a41db76b3ba7128d5789a4ab
Lucas Otoni <lotoni.vivo@gmail.com>
2023-03-31 16:43:33 -0300

    fix: index router

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -2,7 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router';
 import * as components from '../views';
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory(),
   routes: [
     {
       path: '/',
