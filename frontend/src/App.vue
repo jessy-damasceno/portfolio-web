b57da62c25a7290c069da14b23adc97433b6c6cf
Jessy Damasceno <jessy_bas@hotmail.com>
2023-04-05 16:06:46 -0300

    fix: fetch to my user

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -8,13 +8,13 @@ import { onBeforeMount } from 'vue';
 onBeforeMount(() => {
   try {
     axios
-      .get('https://api-portfolio-xi30.onrender.com/user/6421f658ea7240b7b4b5010b')
+      .get('https://api-portfolio-xi30.onrender.com/user/641de57b4a585af300538310')
       .then((response) => {
         localStorage.setItem('user', JSON.stringify(response.data));
       });
 
     axios
-      .get('https://api-portfolio-xi30.onrender.com/project/search?author=lucas')
+      .get('https://api-portfolio-xi30.onrender.com/project/search?author=jessy')
       .then((response) => {
         localStorage.setItem('projects', JSON.stringify(response.data));
       });
